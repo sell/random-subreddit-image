c64468d9257b54749b9f33bba5bce68de3a7db4d
Jay <34258275+sell@users.noreply.github.com>
2020-10-16 20:06:45 -0400

    👋👋👋👋👋

--- server/src/controllers/reddit.js
+++ server/src/controllers/reddit.js
@@ -4,10 +4,8 @@ const axios = require('axios');
 const searchReddit = async (subreddit) => {
     const res = await axios.get(`${redditUrl}${subreddit}.json`);
     return res.data;
-
 }
 
 const randomImage = (arr) => arr[Math.floor(Math.random() * arr.length)];
-
 
 module.exports = { searchReddit, randomImage };
